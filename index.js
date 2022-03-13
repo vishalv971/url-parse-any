dbac6137588c0c1c4e70b26ff9bb581daceeccda
Vishal Vishwanathan <vishalviswanathan9@gmail.com>
2022-03-13 23:26:08 +0530

    Parsing up until subdomain

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-module.exports = function execute() {
-    return "Hello World!"
-};
+function buildSubDomain(subDomainArray) {
+    let subDomainString = "";
+    subDomainArray.map((subDomain) => {
+        subDomainString += subDomain + ".";
+    });
+
+    return subDomainString.substring(0, subDomainString.length - 1);
+}
+
+function parse(url) {
+    let returnedUrlObject = {};
+    let splitURLForProtocol = url.split("://");
+    returnedUrlObject.url = splitURLForProtocol[0];
+    let restOftheURL = splitURLForProtocol[1];
+    returnedUrlObject.hostname = restOftheURL.split("/", 1)[0];
+    let hostname = returnedUrlObject.hostname;
+
+    domainSplitArray = hostname.split(".");
+    returnedUrlObject.tld = domainSplitArray[domainSplitArray.length - 1];
+    returnedUrlObject.domain =
+        domainSplitArray[domainSplitArray.length - 2] +
+        "." +
+        returnedUrlObject.tld;
+
+    returnedUrlObject.subDomain = buildSubDomain(
+        domainSplitArray.slice(0, domainSplitArray.length - 2)
+    );
+    console.log(returnedUrlObject);
+}
+
+parse("https://github.com/foo/bar");
